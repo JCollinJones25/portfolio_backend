760c09c0462726ae7bcb7f644588a149ad2ac8cf
JCollinJones25 <jamescollin.jones.jcj@gmail.com>
2022-05-11 12:55:04 -0400

    backend is done

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const projects = require('./projects.json')
+const about = require('./about.json')
+
+const app = express()
+const PORT = process.env.PORT
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello there')
+})
+
+app.get('/projects', (req, res) => {
+    res.json(projects)
+})
+
+app.get('/about', (req, res) => {
+    res.json(about) 
+})
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on port - ${PORT} ✔`)
+})
